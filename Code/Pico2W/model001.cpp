aa4f36a94b4ce65b367585a589b61bdb3a6c1b82
Luca Anthony <lucaoanthony@iCloud.com>
2026-09-13 12:49:50 -0500

    Started Code for Pico and made Animation directory

--- Code/Pico2W/model001.cpp
+++
@@ -1,1 +0,0 @@
-// This is the first model for the Raspberry Pi Pico2W
---
+++ Code/RaspPiPico/model001.cpp
@@ -0,0 +1,17 @@
+// This is the first model for the Raspberry Pi Pico2W
+
+// =========================================================
+// S.E.N.S.E. - Spatial Echo Navigation & Sensing Equipment
+// model001.cpp - Made for Raspberry Pi Pico
+// =========================================================
+
+// ===============
+//    Includes
+// ===============
+
+
+
+// ====================
+//  INPUTS & VARIABLES
+// ====================
+const int MOT_PIN = 
